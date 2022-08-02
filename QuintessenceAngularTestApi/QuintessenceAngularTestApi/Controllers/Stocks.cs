d36ebe5179d0a5a39d13322d2597ce499f372505
Brandon <brandon@quintessencemail.com>
2022-08-02 08:44:44 +0200

    Angular Test commit

---
+++ QuintessenceAngularTestApi/QuintessenceAngularTestApi/Controllers/Stocks.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuintessenceAngularTestApi.Models;
+using System;
+using System.Collections.Generic;
+
+namespace QuintessenceAngularTestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Stocks : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<StockParent> Get()
+        {
+            try
+            {
+                return StockExtractor.LoadJsonStocks();
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<StockChild> Post(int id)
+        {
+            try
+            {
+                return StockExtractor.LoadJsonStockValues(id);
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
+        }
+    }
+}
